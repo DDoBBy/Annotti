d8d0db392a448e399ed9ba1df48202b2b8c95f77
Euiyeon-Kim <keywi9811key@gmail.com>
2020-07-10 18:29:48 +0900

    Available thumbnail

--- renderers/commons.js
+++ renderers/commons.js
@@ -2,50 +2,52 @@
 let $ = require('jquery')
 const { remote } = require('electron')
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
+let id = 0
 
-let imgPaths = []
-
-
-function fileWalker(dir, ext, done) {
-  let results = []
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err)
-    var pending = list.length
-    if (!pending) return done(null, results)
-    list.forEach(function(file){
-      file = path.resolve(dir, file)
-      fs.stat(file, function(err, stat){
-        if (stat && stat.isDirectory()) {
-          results.push(file)
-          fileWalker(file, ext, function(err, res){
-              results = results.concat(res)
-              if (!--pending) done(null, results)
-          })
-        } else {
-          if(ext.includes(path.extname(file))){
-            results.push(file);
-            imgPaths = imgPaths.concat(file)
-            $('#test-imgs').append("<h3 id=selected-img-"+imgPaths.length+">"+file+"</h3>")
-          }
-          if (!--pending) done(null, results)
-        }
-      })
-    })
-  })
+function composeImgElements(filePath, thumbnailId){
+  let element = '<div class="thumbnail" id="thumbnail-'+thumbnailId+'">'+
+  '<img src="'+filePath+'" style="display: block;width:50px; height:50px"></img>'+
+  '<a class"img-name">'+path.basename(filePath)+"</a></div>"
+  $('#all-imgs').append(element)
 }
 
-function loadImg(){
-  const imgExtensions = ['.png', '.jpg', '.jpeg']
-  let workingDirectory = remote.getGlobal('workingDirectory')    
-    
-  for(i=0;i<workingDirectory.length;i++){
-    fileWalker(workingDirectory[i], imgExtensions, function(err, data){
-      if(err)
-          throw err;
-      imgPaths = imgPaths.concat(data)
-    });
+async function* fileWaker(ext, dir) {
+  const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = path.resolve(dir, dirent.name);
+    if (dirent.isDirectory())
+      yield* fileWaker(ext, res);
+    else {
+      if(ext.includes(path.extname(res))){
+        composeImgElements(res, id++)
+        yield res;
+      }
+    }
   }
 }
 
-$(document).ready(loadImg);
+async function getDataPathFromDir(ext, dir){
+  let dataPaths = []
+  for await (const f of fileWaker(ext, dir))
+    dataPaths.push(f)
+  return dataPaths
+}
+
+async function searchSelectedDirs(ext){
+  let dataPaths = []
+  let workingDirectory = remote.getGlobal('workingDirectory')
+  for (i=0;i<workingDirectory.length;i++){
+    let paths = await getDataPathFromDir(ext, workingDirectory[i])
+    dataPaths = dataPaths.concat(paths)
+  }
+  return dataPaths
+}
+
+async function getAllDataPaths(){
+  const imgExtensions = ['.png', '.jpg', '.jpeg']
+  let test = await searchSelectedDirs(imgExtensions)
+  console.log(test) // 모든 이미지 경로 array 필요한 사람 이 뒤로부터 쓰면 될듯
+}
+
+$(document).ready(getAllDataPaths);
