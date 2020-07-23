e997020b8c62753bf179a64cff5a5cffc455f24c
HanSangMee <ludia1234@naver.com>
2020-07-23 23:00:28 +0900

    Create draw-image.js

---
+++ renderers/draw-image.js
@@ -0,0 +1,183 @@
+let $ = require('jquery')
+const fs = require('fs')
+const path = require('path')
+const { remote } = require('electron')
+
+let id;
+
+function getThumbnailId(){
+    console.log("HELLOO");
+    var tmp = location.href.split("?");
+    var data = tmp[1].split("=");
+    id = data[1];
+    console.log(id);
+    getImageCanvas(id);
+}
+
+function getImageCanvas(thumbnailId){
+    var filePath = remote.getGlobal('projectManager').dataPaths[thumbnailId];
+    drawImageOnCanvas(filePath);
+}
+
+$(document).ready(getThumbnailId)
+
+// 줌, 패닝 기능 되는 캔버스 로드
+function drawImageOnCanvas(filePath){
+    var img = new Image();
+    img.src = filePath; 
+  
+    var canvas = document.getElementById('img-canvas');
+    var ctx = canvas.getContext('2d');
+    trackTransforms(ctx);
+  
+    // var w = $('#all-imgs').width();
+    // var h = $('#all-imgs').height();
+    // canvas.width = w;
+    // canvas.height = h;
+    // img.addEventListener('load', function(){
+    //   var ratio = this.height / this.width;
+    //   //if(ratio < 1.0) canvas.height = w * ratio;
+    //   canvas.width = h * (1 / ratio);
+    // }, false);
+    
+    function redraw(){
+      
+      // Clear the entire canvas
+      var p1 = ctx.transformedPoint(0,0);
+      var p2 = ctx.transformedPoint(canvas.width,canvas.height);
+      ctx.clearRect(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
+  
+      ctx.save();
+      ctx.setTransform(1,0,0,1,0,0);
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      ctx.restore();
+  
+      ctx.drawImage(img,0,0);
+  
+    }
+    redraw();
+  
+    var lastX=canvas.width/2, lastY=canvas.height/2;
+  
+    var dragStart,dragged;
+  
+    // 마우스 클릭한 순간
+    canvas.addEventListener('mousedown',function(evt){
+      document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+      lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+      lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+      dragStart = ctx.transformedPoint(lastX,lastY);
+      dragged = false;
+    },false);
+  
+    // 클릭해서 움직이는 순간
+    canvas.addEventListener('mousemove',function(evt){
+      lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+      lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+      dragged = true;
+      if (dragStart){
+        var pt = ctx.transformedPoint(lastX,lastY);
+        ctx.translate(pt.x-dragStart.x,pt.y-dragStart.y);
+        redraw();
+      }
+    },false);
+  
+    // 마우스에서 손 떼는 순간
+    canvas.addEventListener('mouseup',function(evt){
+      dragStart = null;
+      //if (!dragged) zoom(evt.shiftKey ? -1 : 1 );
+    },false);
+  
+    var scaleFactor = 1.1;
+  
+    var zoom = function(clicks){
+      var pt = ctx.transformedPoint(lastX,lastY);
+      ctx.translate(pt.x,pt.y);
+      var factor = Math.pow(scaleFactor,clicks);
+      ctx.scale(factor,factor);
+      ctx.translate(-pt.x,-pt.y);
+      redraw();
+    }
+  
+    // 확대 
+    $('#zoom-in-button').on('click', function(){
+      zoom(1);
+    });
+  
+    // 축소
+    $('#zoom-out-button').on('click', function(){
+      zoom(-1);
+    });
+  
+    // 스크롤 올리면 확대 내리면 축소
+    var handleScroll = function(evt){
+      var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
+      if (delta) zoom(delta);
+      return evt.preventDefault() && false;
+    };
+  
+    canvas.addEventListener('DOMMouseScroll',handleScroll,false);
+    canvas.addEventListener('mousewheel',handleScroll,false);
+  }
+  
+function trackTransforms(ctx){
+    var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+    var xform = svg.createSVGMatrix();
+    ctx.getTransform = function(){ return xform; };
+
+    var savedTransforms = [];
+    var save = ctx.save;
+    ctx.save = function(){
+        savedTransforms.push(xform.translate(0,0));
+        return save.call(ctx);
+    };
+
+    var restore = ctx.restore;
+    ctx.restore = function(){
+        xform = savedTransforms.pop();
+        return restore.call(ctx);
+    };
+
+    var scale = ctx.scale;
+    ctx.scale = function(sx,sy){
+        xform = xform.scaleNonUniform(sx,sy);
+        return scale.call(ctx,sx,sy);
+    };
+
+    var rotate = ctx.rotate;
+    ctx.rotate = function(radians){
+        xform = xform.rotate(radians*180/Math.PI);
+        return rotate.call(ctx,radians);
+    };
+
+    var translate = ctx.translate;
+    ctx.translate = function(dx,dy){
+        xform = xform.translate(dx,dy);
+        return translate.call(ctx,dx,dy);
+    };
+
+    var transform = ctx.transform;
+    ctx.transform = function(a,b,c,d,e,f){
+        var m2 = svg.createSVGMatrix();
+        m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
+        xform = xform.multiply(m2);
+        return transform.call(ctx,a,b,c,d,e,f);
+    };
+
+    var setTransform = ctx.setTransform;
+    ctx.setTransform = function(a,b,c,d,e,f){
+        xform.a = a;
+        xform.b = b;
+        xform.c = c;
+        xform.d = d;
+        xform.e = e;
+        xform.f = f;
+        return setTransform.call(ctx,a,b,c,d,e,f);
+    };
+
+    var pt  = svg.createSVGPoint();
+    ctx.transformedPoint = function(x,y){
+        pt.x=x; pt.y=y;
+        return pt.matrixTransform(xform.inverse());
+    }
+}
--- renderers/tab-functions.js
+++ renderers/tab-functions.js
@@ -7,12 +7,12 @@ function readyTab(){
 }
 
 function openTab(thumbnailId){
-    console.log(thumbnailId);
+    //console.log(thumbnailId);
     var filePath = remote.getGlobal('projectManager').dataPaths[thumbnailId];
     var basename = path.basename(filePath);
-    if (basename.length > 10){
-        basename = basename.slice(0, 5) + "..." + basename.slice(-5);
-    }
+    // if (basename.length > 10){
+    //     basename = basename.slice(0, 5) + "..." + basename.slice(-5);
+    // }
 
 
     $('#working-area').css("display", "none");
@@ -22,9 +22,13 @@ function openTab(thumbnailId){
 
     tab = tabGroup.addTab({
         title: basename,
-        src: "../templates/tab.html",
+        src: "../templates/tab.html?id=" + thumbnailId,
         visible: true,
         active: true
+    });
+
+    tab.on("webview-ready", (tab) => {
+        
     });
 
     tab.on("close", (tab) => {
