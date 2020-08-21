43569e29405b4a46e388f3345a889520975a27ad
Euiyeon-Kim <keywi9811key@gmail.com>
2020-08-22 03:32:48 +0900

    Drag function for classification

--- app/renderers/classification.js
+++ app/renderers/classification.js
@@ -1,43 +1,42 @@
 const { getImageCanvas } = require('../renderers/draw-image');
 
-$('.label-infos').on('click', '.activate', function (event) {
-  if ($(event.target).prop('checked')) {
-    $('[class=activate]').prop('checked', false);
-    $(event.target).prop('checked', true);
-    remote.getGlobal('projectManager').activateLabel($(event.target).attr('id'));
-  } else remote.getGlobal('projectManager').activateLabel(null);
+const selection = Selection.create({
+  class: 'selection',
+  selectables: ['#all-imgs > div'],
+  boundaries: ['#all-imgs'],
 });
 
-$('.working-area').on('click', '.thumbnail', function (event) {
-  if (event.altKey) {
-    // Ctrl key doesn't working at MAC OS
-    if (remote.getGlobal('projectManager').activated == null)
-      alertError('Label selection error', 'You need to select label before assign it to data');
-    else {
-      // Annotation
-      var fileID = $(event.target).attr('id');
-      var filePath = $(event.target).attr('src');
-      var labelID = remote.getGlobal('projectManager').activated;
-      var labelColor = remote.getGlobal('projectManager').getColorbyLabelID(labelID);
-      var work = remote.getGlobal('projectManager').selectWork(labelID, fileID);
+selection
+  .on('beforestart', (evt) => {
+    if (!evt.oe.altKey) return false;
+    var labelID = remote.getGlobal('projectManager').activated;
+    if (labelID == null) return false;
+  })
+  .on('move', ({ changed: { added } }) => {
+    var labelID = remote.getGlobal('projectManager').activated;
+    var labelColor = remote.getGlobal('projectManager').getColorbyLabelID(labelID);
 
+    for (const el of added) {
+      if (el.className == 'folder-info') continue;
+      var fileID = el.id;
+      var filePath = el.children[0].src;
+      var work = remote.getGlobal('projectManager').selectWork(labelID, fileID);
       if (work == 'append') {
         remote.getGlobal('projectManager').appendFiletoLabel(labelID, fileID, filePath);
-        $(event.target).css({ border: '8px solid' + labelColor });
+        el.children[0].style.cssText = 'border: 8px solid' + labelColor + ';';
         $('#' + labelID + '.label-counter').text(
           Number($('#' + labelID + '.label-counter').text()) + 1
         );
       } else if (work == 'delete') {
         remote.getGlobal('projectManager').deleteFilefromLabel(labelID, fileID, filePath);
-        $(event.target).css({ border: 'none' });
+        el.children[0].style.cssText = 'border: none;';
         $('#' + labelID + '.label-counter').text(
           Number($('#' + labelID + '.label-counter').text()) - 1
         );
       } else {
-        // 'change'
         prevLabelID = remote
           .getGlobal('projectManager')
-          .getLabelIDbyColor(rgb2hex($(event.target).css('border-color')));
+          .getLabelIDbyColor(rgb2hex(el.children[0].style.borderColor));
         $('#' + prevLabelID + '.label-counter').text(
           Number($('#' + prevLabelID + '.label-counter').text()) - 1
         );
@@ -45,17 +44,20 @@ $('.working-area').on('click', '.thumbnail', function (event) {
           Number($('#' + labelID + '.label-counter').text()) + 1
         );
         remote.getGlobal('projectManager').changeFileLabel(labelID, fileID, filePath);
-        $(event.target).css({ border: '8px solid' + labelColor });
+        el.children[0].style.cssText = 'border: 8px solid' + labelColor + ';';
       }
     }
-  } else {
-    var fileID = $(event.target).attr('id');
-    var filePath = $(event.target).attr('src');
-    var basename = path.basename(filePath);
-    $('#classification-file-name').text(basename);
-    $('.classification-zoom-window').css('display', 'block');
-    getImageCanvas(fileID);
-  }
+  })
+  .on('stop', ({ inst }) => {
+    inst.keepSelection();
+  });
+
+$('.label-infos').on('click', '.activate', function (event) {
+  if ($(event.target).prop('checked')) {
+    $('[class=activate]').prop('checked', false);
+    $(event.target).prop('checked', true);
+    remote.getGlobal('projectManager').activateLabel($(event.target).attr('id'));
+  } else remote.getGlobal('projectManager').activateLabel(null);
 });
 
 $('.classification-close-btn').on('click', () => {
